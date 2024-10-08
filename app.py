d4518c6bc88ddea264065794575873820227d3c8
Nameless Coder <110830345+Haarinaath-Y@users.noreply.github.com>
2024-10-08 17:25:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,26 +6,16 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
 
 # Google Drive authentication
 def authenticate_gdrive():
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    return creds
+    creds = Credentials.from_service_account_info(st.secrets["gdrive"])
+    service = build('drive', 'v3', credentials=creds)
+    return service
 
 
 # Download SQLite database from Google Drive
